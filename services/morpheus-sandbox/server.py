25b14de5acdeb10afba232ac46e92bf0ac791517
Hung <hungnq@Hungs-MacBook-Air.local>
2025-12-25 22:48:32 +0700

    fix bug port

--- services/morpheus-sandbox/server.py
+++ services/morpheus-sandbox/server.py
@@ -46,7 +46,7 @@ class StatusRequest(BaseModel):
     project_id: str
 
 # Backend API URL for updating file records
-BACKEND_API_URL = "http://localhost:5000"
+BACKEND_API_URL = os.environ.get("BACKEND_API_URL", "http://localhost:8080")
 MORPHEUS_API_KEY = os.environ.get("MORPHEUS_API_KEY", "dev-secret-key")
 
 logger.info(

788ef159ab30c5e7e8a1c90d51d7845546b4ff3d
tr1la <phucuongto2004@gmail.com>
2026-04-02 00:07:08 +0700

    Revert "Clean old workflow and upd new workflow exec"
    This reverts commit 13649d417976524ccb7fb4f793021e7da16e3ebc.

--- services/morpheus-sandbox/server.py
+++ services/morpheus-sandbox/server.py
@@ -14,7 +14,7 @@ from pydantic import BaseModel
 import aiohttp
 import requests
 
-from morpheus.workflows.analyze_csv.state_graph import StatefulAnalyzeCSVWorkflow
+from morpheus.workflows.analyze_csv.workflow import AnalyzeCSVWorkflow
 from utils.config import config
 from utils.dynamodb import save_dashboard_metadata
 from utils.health import check_health
